0edd698b900db54426361beebb5ae88735d6e7bf
kapinga <erickfloriana@gmail.com>
2024-12-29 08:49:16 -0800

    variables conversion

--- substraction.cpp
+++ substraction.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 int main()
 {
-    int students = 15;
+    double students = (int)15.6;
     // students = students - 100;
     // students = students * 2;
     // students *= 2
     // students = students / 2;
     // students /= 2;
-    int remainder = students % 2;
     std::cout
-        << remainder;
+        << students;
 }
