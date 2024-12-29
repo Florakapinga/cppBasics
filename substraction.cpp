b72a361991770054dc7e407948a360516eeb5c7b
kapinga <erickfloriana@gmail.com>
2024-12-29 08:21:24 -0800

    another method

--- substraction.cpp
+++ substraction.cpp
@@ -3,6 +3,7 @@ int main()
 {
     int students = 1000;
     // students = students - 100;
-    students = students * 2;
+    // students = students * 2;
+    students *= 2;
     std::cout << students;
 }
