0949909035cb3829c35386fe7e898af67c9c7ed7
kapinga <erickfloriana@gmail.com>
2024-12-29 08:38:15 -0800

    reminder

--- substraction.cpp
+++ substraction.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 int main()
 {
-    int students = 1000;
+    int students = 15;
     // students = students - 100;
     // students = students * 2;
     // students *= 2
     // students = students / 2;
-    students /= 2;
-    std::cout << students;
+    // students /= 2;
+    int remainder = students % 2;
+    std::cout
+        << remainder;
 }
